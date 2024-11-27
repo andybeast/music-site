a8826939927f3db910e08390d419c0c084e0dfa3
andybeast <andreasfhotvedt@hotmail.com>
2024-11-27 01:57:33 +0100

    last fix

---
+++ app/store/page.tsx
@@ -0,0 +1,18 @@
+// Import necessary components and libraries
+
+'use client'
+
+
+// Define an array of steps for the map
+
+
+export default function Login() {
+  return (
+      
+    <div className="flex">
+      
+
+
+    </div>
+  );
+}
